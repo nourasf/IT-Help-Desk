b011d5d591ba293b23fe62e6d3ad44062da02197
nourasf <202311967@ua.edu.lb>
2026-07-31 13:25:15 +0300

    Add admin-only user creation

---
+++ backend/DTOs/Users/CreateUserRequestDto.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.DTOs.Users;
+
+public class CreateUserRequestDto
+{
+    [Required]
+    [MaxLength(100)]
+    public string FullName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(255)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(8)]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    public string Role { get; set; } = string.Empty;
+}
