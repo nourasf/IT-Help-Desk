bd71171afa695c4a3ff9b4e1c2ec810b006a4a58
nourasf <202311967@ua.edu.lb>
2026-08-14 00:31:44 +0300

    Load final ticket action polish styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import "./styles/App.css";
 import "./styles/Auth.css";
 import "./styles/TicketDetailsEnhancements.css";
+import "./styles/TicketActionPolish.css";
 import { BrowserRouter, Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import Notifications from "./pages/notifications/Notifications";
 import Login from "./pages/auth/Login";
