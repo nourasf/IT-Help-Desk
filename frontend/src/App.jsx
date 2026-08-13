92e10b4c350b2bac714ff812f9066153f7a68799
nourasf <202311967@ua.edu.lb>
2026-08-14 00:48:30 +0300

    Load stronger ticket contrast styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./styles/Auth.css";
 import "./styles/TicketDetailsEnhancements.css";
 import "./styles/TicketActionPolish.css";
 import "./styles/TicketActionHotfix.css";
+import "./styles/ContrastPass.css";
 import "./utils/globalDashboardFixes.js";
 import { BrowserRouter, Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import Notifications from "./pages/notifications/Notifications";
