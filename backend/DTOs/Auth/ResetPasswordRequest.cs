575764308bd311482b917f16f2510882a43004f5
nourasf <202311967@ua.edu.lb>
2026-08-13 03:36:25 +0300

    Update reset password request for OTP

--- backend/DTOs/Auth/ResetPasswordRequest.cs
+++ backend/DTOs/Auth/ResetPasswordRequest.cs
@@ -2,7 +2,7 @@ using backend.DTOs.Auth;
 
 public class ResetPasswordRequest
 {
-    public String Token {get;set;}= string.Empty;
-    public String NewPassword {get;set;}=string.Empty;
-    
+    public string Email { get; set; } = string.Empty;
+    public string Otp { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
 }
