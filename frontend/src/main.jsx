8bffdbd0db4919848dd9b658efbb8b2b359854fc
nourasf <202311967@ua.edu.lb>
2026-08-20 23:46:44 +0300

    Load global product theme before page-specific styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./styles/global/index.css";
+import "./styles/global/ProductTheme.css";
 import "./styles/tickets/CreateTicket.css";
 import App from "./App.jsx";
 
